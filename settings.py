59a73d6c0a2f66183a795ac7f17a758344215125
schaaya <119798775+schaaya@users.noreply.github.com>
2023-08-07 17:44:04 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 
 
-SEARCH_KEY = "AIzaSyAzr3OO6CH32dDUMtowaQZqIvUoeZUTFpQ"
-SEARCH_ID = "d79b197dd41634cab"
-OPENAI_API_KEY = "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"
+SEARCH_KEY = ""
+SEARCH_ID = ""
+OPENAI_API_KEY = ""
 COUNTRY = "us"
 SEARCH_URL = "https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&num=10&gl=" + COUNTRY
 RESULT_COUNT = 10
